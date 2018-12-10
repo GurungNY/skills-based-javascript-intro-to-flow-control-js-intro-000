2e8e1b68ccc578ca02480fc96efcc6e94a9cd197
Yosuf Gurung <yosufgurung@gmail.com>
2018-12-10 20:56:30 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,11 +5,22 @@ if(13 <= age && age <= 19) {
 }
 
 function teenager(age) {
-
+if(13 <= age && age <= 19) {
+  return 'You are a teenager!'
+} else {
+  (13 > age && age >19)
+  return  'You are not a teenager'
+}
 }
 
 function ageChecker(age) {
-
+  if(13 <= age && age <= 19) {
+    return 'You are a teenager!'
+  } else if (age <= 12) {
+    return 'You are a kid'
+  } else if (20 <= age) {
+    return 'You are a grownup'
+}
 }
 
 function ternaryTeenager(age) {
